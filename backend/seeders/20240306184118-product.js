1a44817c2e7e1995d7b00888b40a8bb884eb337b
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2024-03-06 15:48:53 -0300

    :heavy_plus_sign: build: Seeder adicionado para o banco de dados "Products"

---
+++ backend/seeders/20240306184118-product.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert('Products', [
+      // {
+      //   name: '',
+      //   brand: '',
+      //   model: '',
+      //   price: 0,
+      //   color: '',
+      // },
+      {
+        name: 'Xiaomi Redmi 9s',
+        brand: 'Xiaomi',
+        model: 'Redmi 9s',
+        price: 1279,
+        color: 'blue sky',
+      },
+      {
+        name: 'Samsung Galaxy Note 10 Plus',
+        brand: 'Samsung',
+        model: 'Galaxy Note 10 Plus',
+        price: 7999,
+        color: 'black',
+      },
+      {
+        name: 'Apple Iphone 14 Pro',
+        brand: 'Apple',
+        model: 'Iphone 14 Pro',
+        price: 12999,
+        color: 'white',
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Products', null, {});
+  },
+};
