728c97056fe88c9ccf767085f0c2528efab745bb
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2024-03-06 15:57:19 -0300

    :wrench: chore: Ajuste no banco de dados para postgres

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
     port: process.env.DB_PORT,
   },
   test: {
@@ -14,7 +14,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
     port: process.env.DB_PORT,
   },
   production: {
@@ -22,7 +22,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
     port: process.env.DB_PORT,
   },
 };
