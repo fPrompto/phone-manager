d3cb730d450ec81f738a67d1287423db702144f7
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2024-03-07 11:14:53 -0300

    :sparkles: feat: Função para criar um novo produto no banco de dados adicionado ao backend

--- backend/services/productService.js
+++ backend/services/productService.js
@@ -5,10 +5,36 @@ const findAll = async () => {
   const find = await Product.findAll();
   return {
     status: STATUS_CODE.OK,
+    message: '',
     data: find,
   };
 };
 
+const create = async ({ name, brand, model, price, color }) => {
+  try {
+    const create = await Product.create({
+      name,
+      brand,
+      model,
+      price,
+      color,
+    });
+
+    return {
+      status: STATUS_CODE.CREATED,
+      message: PRODUCT_MESSAGE.CREATE.SUCCESS,
+      data: create,
+    };
+  } catch (e) {
+    return {
+      status: STATUS_CODE.BAD_REQUEST,
+      message: PRODUCT_MESSAGE.CREATE.FAIL,
+      data: '',
+    };
+  }
+};
+
 module.exports = {
   findAll,
+  create,
 };
