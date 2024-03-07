28a0e0aed1e5f872580e4ea94864a82a650b0efe
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2024-03-07 11:45:06 -0300

    :bug: fix: Correçao no model product pare que gere um id automaticamente ao criar um produto

--- backend/models/product.js
+++ backend/models/product.js
@@ -26,10 +26,10 @@ module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define(
     'Product',
     {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-      },
+      // id: {
+      //   type: DataTypes.INTEGER,
+      //   primaryKey: true,
+      // },
       name: DataTypes.STRING,
       brand: DataTypes.STRING,
       model: DataTypes.STRING,
