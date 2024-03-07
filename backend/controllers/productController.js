366f2b8e45328e2d74e124760d8cf3105761c1df
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2024-03-07 11:01:43 -0300

    :wrench: chore: Controller modificado para o novo padrão de resposta

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,9 @@ const productService = require('../services/productService');
 
 const findAll = async (_req, res) => {
   const find = await productService.findAll();
-  return res.status(find.status).json(find.data);
+  return res
+    .status(find.status)
+    .json({ message: find.message, data: find.data });
 };
 
 module.exports = {
